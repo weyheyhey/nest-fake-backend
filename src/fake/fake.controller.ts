09c252ef8c69fd826d0426155ec9403149d8e83e
Dr0kD3n <badalov.e@uremont.com>
2020-02-19 12:22:55 +0300

    feat(src/fake): fake-news swagger

--- src/fake/fake.controller.ts
+++ src/fake/fake.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, HttpCode } from "@nestjs/common";
+import { ApiTags, ApiOkResponse } from "@nestjs/swagger";
 
+@ApiTags("fake")
 @Controller("fake")
 export class FakeController {
   @Get("news")
   @HttpCode(200)
+  @ApiOkResponse({ description: "Successful operation" })
   fake() {
     return {
       username: "admin",
